05bdca90fdb44ebad608a708177afbcb661b1aff
City of Prescott <webmaster@prescott-az.gov>
2014-05-30 13:53:09 -0700

    2014.05.30

--- header.php
+++ header.php
@@ -20,9 +20,9 @@
 
 		<!-- icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) -->
 		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-icon-touch.png">
-		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico">
 		<!--[if IE]>
-			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico">
 		<![endif]-->
 		<!-- or, set /favicon.ico for IE10 win -->
 
